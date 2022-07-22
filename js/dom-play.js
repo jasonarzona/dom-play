364ac6140870ab576e431c2d909c25a607ccc2f8
jasonarzona <jarjona@sea.yearup.org>
2022-07-21 23:42:30 -0700

    finished scene

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,7 +1,35 @@
-// let actors = document.querySelectorAll(".hamlet");
+function myAlert(){
+    alert("I'm clicked!")
+}
 
-// console.log(actors);
+const spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    // mySpan.addEventListener("click",myAlert);
+
+    mySpan.addEventListener("click",function(ev){
+        // alert("The current actor is " + mySpan.dataset.actor);
+        highlightActor(mySpan.dataset.actor);
+    });
+}
+
+function highlightActor(actor){
+
+    for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor='yellow';
+        }else{//other actor, background white
+            mySpan.style.backgroundColor='white';
+        }
+    }
+}
 
 function highlight(el){
-    el.style.backgroundColor='yellow';
+    if(el.style.backgroundColor=='yellow'){
+        el.style.backgroundColor='white';
+    }else{
+        el.style.backgroundColor='yellow';
+    }
 }
